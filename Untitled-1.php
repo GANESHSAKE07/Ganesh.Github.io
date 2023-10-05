c6471558e23acd1bfe6ea439b5bdf2e484ab599c
GaneshSake <142732693+GANESHSAKE07@users.noreply.github.com>
2023-10-05 12:12:25 +0530

    Add files via upload
    Initial Commit

---
+++ Untitled-1.php
@@ -0,0 +1,162 @@
+<!DOCTYPE HTML>
+<!--
+	Massively by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Massively by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="fade-in">
+
+				<!-- Intro -->
+					<div id="intro">
+						<h1>Ganesh Port website<br />
+						</h1>
+						<p>Data Analyst skilled in Sql,Powerbi,Python,Excel <a href="https://www.linkedin.com/in/ganesh-sake/">@www.linkedin.com/in/ganesh-sake</a> 
+						
+						<ul class="actions">
+							<li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+						</ul>
+					</div>
+
+				<!-- Header -->
+					<header id="header">
+						<a href="index.html" class="logo">ganesh the analyst</a>
+					</header>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul class="links">
+							<li class="active"><a href="index.html">projects</a></li>
+							
+						</ul>
+						<ul class="icons">
+				
+							<li><a href="https://www.linkedin.com/in/ganesh-sake/" class="icon brands fa-linkedin"><span class="label">>linkedin</span></a></li>
+							<li><a href="https://github.com/GANESHSAKE07?tab=repositories" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Featured Post -->
+							<article class="post featured">
+								<header class="major">
+									
+									<h2><a href="https://github.com/GANESHSAKE07/SQL-PROJECT">Data cleaning in SQL project on<br />
+									</a></h2>
+									<p>in this project we cleaned data,filtered data in sql server.</p>
+								</header>
+								<a href="https://github.com/GANESHSAKE07/SQL-PROJECT" class="image main"><img src="images/covid.jpg" alt="" /></a>
+								<ul class="actions special">
+									<li><a href="https://github.com/GANESHSAKE07/SQL-PROJECT" class="button large">view project</a></li>
+								</ul>
+							</article>
+
+						<!-- Posts -->
+							<section class="posts">
+								<article>
+									<header>
+										
+										<h2><a href="https://github.com/GANESHSAKE07/POWERBI-PROJECT-REPORT">powerbi project and dash board<br />
+										</a></h2>
+									</header>
+									<a href="https://github.com/GANESHSAKE07/POWERBI-PROJECT-REPORT" class="image fit"><img src="images/powerbi.jpg" alt="" /></a>
+									<p>created reports and dashboards by using public data sets and did data modelling
+									<ul class="actions special">
+										<li><a href="https://github.com/GANESHSAKE07/POWERBI-PROJECT-REPORT" class="button">view project</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+										
+						<!-- Posts -->
+							<section class="posts">
+								<article>
+									<header>
+										
+										<h2><a href="https://github.com/GANESHSAKE07/python_EDA">Python EDA Project<br />
+										</a></h2>
+									</header>
+									<a href="https://github.com/GANESHSAKE07/python_EDA" class="image fit"><img src="images/python.jpg" alt="" /></a>
+									<p>Data manipulation and exploratory Data Analysis done by Data Visualization librariesbyusing Numpy,Pandas,Matplotlib
+									<ul class="actions special">
+										<li><a href="https://github.com/GANESHSAKE07/python_EDA" class="button">view project</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+						<!-- Posts -->
+							<section class="posts">
+								<article>
+									<header>
+										
+										<h2><a href="https://github.com/GANESHSAKE07/Exceldashboard">Excel Project<br />
+										</a></h2>
+									</header>
+									<a href="https://https://github.com/GANESHSAKE07/Exceldashboard" class="image fit"><img src="images/Excel.png" alt="" /></a>
+									<p>Data Cleaning and Integration done by Advanced Excel methods
+										<li><a href="https://github.com/GANESHSAKE07/Exceldashboard" class="button">view project</a></li>
+									</ul>
+								</article>
+								<article>
+									<header>
+
+										
+										
+			
+
+				
+						<section class="split contact">
+							<section class="alt">
+								<h3>location</h3>
+								<p><br />
+								Ananthapur,Ap, 515133</p>
+							</section>
+							<section>
+								<h3>Phone</h3>
+								<p><a href="#">7676021651
+					
+								</a></p>
+							</section>
+							<section>
+								<h3>Email</h3>
+								<p><a href="#">ganeshsake07@gmail.com</a></p>
+							</section>
+							<section>
+								<h3>linkedin</h3>
+								<a href="https://www.linkedin.com/in/ganesh-sake/">@www.linkedin.com/in/ganesh-sake</a> 
+								<ul class="icons alt">
+									
+							</section>
+						</section>
+					</footer>
+
+				<!-- Copyright -->
+					<div id="copyright">
+						<ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
